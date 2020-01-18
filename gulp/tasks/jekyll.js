50cefbab0816bc2875da0f11d4d85175d171ee4a
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-01-18 13:07:53 +0700

    fix

--- gulp/tasks/jekyll.js
+++ gulp/tasks/jekyll.js
@@ -1,21 +1,20 @@
-const { series, watch, src } = require('gulp');
-const cp = require('child_process');
-const paths = require('../paths');
+const { series, watch, src } = require('gulp')
+const path = require('path')
+const cp = require('child_process')
+const paths = require('../paths')
 
 const buildJekyll = () => {
-    return cp.spawn("bundle", ["exec", "jekyll", "build"],
-        {
-            stdio: 'inherit',
-            cwd: "/Users/vadimmishozev/Desktop/jekyll"
-        }
-    )
-};
+  return cp.spawn('bundle', ['exec', 'jekyll', 'build'], {
+    stdio: 'inherit',
+    cwd: path.resolve(__dirname + '../../../'),
+  })
+}
 
-const watchJekyll = (reload) => {
-    watch([`${paths.html.src}**/*.html`], series(buildJekyll, reload))
-};
+const watchJekyll = reload => {
+  watch([`${paths.html.src}**/*.html`], series(buildJekyll, reload))
+}
 
 module.exports = {
-    watchJekyll,
-    buildJekyll
-};
+  watchJekyll,
+  buildJekyll,
+}
